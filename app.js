9278388926efe9e3280b5e9e373e9ec55bcd8276
Viktor Pangburn <viktor.pangburn@gmail.com>
2025-04-27 16:30:53 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+require('dotenv').config
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/login.html'));
+});
+
+app.get('/signup', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/signUp.html'));
+});
+
+app.route('/login').post((req,res) => {
+    res.redirect('/homepage');
+});
+
+app.route('/register').post((req,res) => {
+    res.redirect('/homepage');
+});
+
+app.get('/homepage', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/homepage.html'));
+});
+
+app.get('/analyze', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/analyze.html'));
+});
+
+app.get('/calender', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/calender.html'));
+});
+
+app.get('/profile', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/profile.html'));
+});
+
+
+
+
+
+app.listen(port, () => {
+    console.log("App listening on port " + port)
+})
